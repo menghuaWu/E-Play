7ac6bba283a7c0c9854ed67c7cb30b0f9cc9c98a
menghuaWu <wumenghua0429@gmail.com>
2020-04-28 22:30:30 +0800

    Init DbContext

---
+++ E-Play/Data/MainCategory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+/// <summary>
+/// 主類別Table
+/// </summary>
+namespace E_Play.Data
+{
+    public class MainCategory
+    {
+        [Key]
+        [DisplayName("編號")]
+        public int Id { get; set; }
+
+
+        [DisplayName("主類別")]
+        public string Name { get; set; }
+
+        public virtual ICollection<SubCategory> SubCategories { get; set; }
+
+    }
+}
