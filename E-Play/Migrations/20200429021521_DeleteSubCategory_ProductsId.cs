76ea0c4c4a5df6b5103f7cba86820416c26fbb97
menghuaWu <wumenghua0429@gmail.com>
2020-04-29 10:19:49 +0800

    重新Migration更新資料庫欄位

---
+++ E-Play/Migrations/20200429021521_DeleteSubCategory_ProductsId.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace E_Play.Migrations
+{
+    public partial class DeleteSubCategory_ProductsId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "SubCategories");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "SubCategories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
